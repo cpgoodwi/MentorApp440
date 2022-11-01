baddb72e815b799d7269158dda4dcafe67f9c20d
cpgoodwi <cpgoodwi@asu.edu>
2022-11-01 10:41:50 -0700

    fixed merge errors

--- MentorApp440/Helpers/SqlConnection.cs
+++ MentorApp440/Helpers/SqlConnection.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Data;
-using System.Windows.Forms;
+// using System.Windows.Forms;
 using System.Data.SqlClient;
-using MySql.Data.MySqlClient;
+// using MySql.Data.MySqlClient;
 using MentorApp440.Models;
-using Microsoft.Data.SqlClient;
+// using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 
 namespace MentorApp440.Helpers;
