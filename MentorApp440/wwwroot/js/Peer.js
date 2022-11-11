0f540aad1dcb4c434e7a68646ce7a31aa7356d40
lujunwei <chunweil@asurite.asu.edu>
2022-11-10 17:43:31 -0700

    Progress Bar Added

--- MentorApp440/wwwroot/js/Peer.js
+++ MentorApp440/wwwroot/js/Peer.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+class Peer {
+    constructor(name) {
+        this.name = name
+    }
+
+    renderHTML() {
+        return (
+            <li>
+                <ul>
+                    <img src="~/lib/Images/Profile.jpeg" className="img-thumbnail" id="peerPic" alt="" width="10px"
+                         height="10px"/>
+                    <a href="">${this.name}</a>
+                </ul>
+            </li>
+        )
+    }
+}
